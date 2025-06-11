18c69bdac64b2467e0f0f35e95d2823fc76989a8
José Carlos Boaretto Netto <boarettonetto@gmail.com>
2025-06-11 10:51:27 -0300

    adc img

--- src/pages/homepage/home.jsx
+++ src/pages/homepage/home.jsx
@@ -1,9 +1,17 @@
 import styles from "./home.module.css";
 export function HomePage() {
-  console.log("HomePage sendo renderizada"); // Debug
+  console.log("Home renderizada");
   return (
-    <div className={styles.home}>
-      <h1>Página Home</h1>
-    </div>
+    <>
+      <div className={styles.home}>
+        <h1>Página Home</h1>
+      </div>
+      <div className={styles.msg}>
+        <p>Desculpa pelas estilizações professora, eu estou sem criatividade.</p>
+      </div>
+      <div className={styles.foto}>
+        <img src="https://i.pinimg.com/736x/db/db/8c/dbdb8c03cbc958e1b878aed2436f0d55.jpg" alt="imagem codigo" />
+      </div>
+    </>
   );
 }
